a7b61fd7e0209b7246e010c5f73a94f9bf4f22ba
Shah zeb <34839860+shahzeb61518@users.noreply.github.com>
2020-06-09 00:29:22 +0500

    cors

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Accept-Encoding,Accept-Language,Connection,Accept,Content-Type,Content-Length,Host,Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', false);
   next();
 });
 
