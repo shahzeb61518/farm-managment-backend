b53dcebb6a13eead3c565a26ad446d80779ecfb9
Shah zeb <34839860+shahzeb61518@users.noreply.github.com>
2020-06-09 01:43:30 +0500

    ...

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ mongoose.connect(url, (err, db) => {
 
 // app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Accept-Encoding,Accept-Language,Connection,Accept,Content-Type,Content-Length,Host,Authorization');
